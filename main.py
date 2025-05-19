c2c1878844a3c25f21b8c61e9540f363b8907f03
ShinHouki <148901615+ShinHouki@users.noreply.github.com>
2025-05-20 03:50:56 +0800

    please approve my submit for partial clear line function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+#setup code
 y = 0
-x = 1
+x = 2
 board = [[0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
@@ -10,10 +11,30 @@ pieces = [[[1, 1], [1, 1]],
     [[1, 1], [0, 1]],
     [[0, 0], [1, 1]]]
 rand = randint(0, 3)
+gameover = False
+randp = pieces[rand]
+#check if line cleared
+
+def checkline():
+    for j in range(len(board[0])):
+        for i in range(len(board)):
+            if(board[i][j] == 0):
+                break
+            else: 
+                if(i == 4 and board[i][j] == 1):
+                    for k in range(5):
+                        led.unplot(k, j)
+                        board[k][j] == 0
+                        pause(200)
+
+
+
 
 def on_forever():
-    global x, y, rand
-    randp = pieces[rand]
+    global x, y, rand, gameover, randp
+    while gameover:
+        basic.clear_screen()
+        basic.show_icon(IconNames.SAD)
     for i in range(len(randp)):
         for j in range(len(randp[i])):
             if(randp[i][j] == 1):
@@ -23,18 +44,83 @@ def on_forever():
             for j in range(len(randp[i])):
                 if(randp[i][j] == 1):
                     led.unplot(x + i, y + j)
-    for j in range(2):
-        if(randp[j][1] == 1 and (y + 1 == 4 or board[x + j][y + 2] == 1)):
-            if(y <= 1): 
-                gameover = True
-            for i in range(len(randp)):
-                for k in range(len(randp[i])):
-                    if(randp[i][k] == 1):
-                        board[x + i][y + k] = 1
-                        led.plot(x + i, y + k)
-            rand = randint(0, 3)
-            y = -1
-            x = 1
-
+    for j in range(len(randp)):
+        for l in range(len(randp[j])):
+            if(randp[j][l] == 1 and (y + l == 4 or board[x + j][y + l + 1] == 1)):
+                if(y < 1): 
+                    gameover = True
+                for i in range(len(randp)):
+                    for k in range(len(randp[i])):
+                        if(randp[i][k] == 1):
+                            board[x + i][y + k] = 1
+                            led.plot(x + i, y + k)
+                rand = randint(0, 3)
+                y = -1
+                x = 2
+                randp = pieces[rand]
+                checkline()
     y += 1
 basic.forever(on_forever)
+
+#left right code
+def on_button_pressed_a():
+    global x, randp
+    move = True
+    for i in range(len(randp)):
+        for j in range(len(randp[i])):
+            if(randp[i][j] == 1 and ( x + i == 0 or board[x + i - 1][y + j] == 1)):
+                move = False
+    if(move == True):
+        for i in range(len(randp)):
+                for j in range(len(randp[i])):
+                    led.unplot(x + i, y + j)
+        x -= 1
+        for i in range(len(randp)):
+            for j in range(len(randp[i])):
+                if(randp[i][j] == 1):
+                    led.plot(x + i, y + j)
+
+
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global x, randp
+    move = True
+    for i in range(len(randp)):
+        for j in range(len(randp[i])):
+            if(randp[i][j] == 1 and ( x + i == 4 or board[x + i + 1][y + j] == 1)):
+                move = False
+    if(move == True):
+        for i in range(len(randp)):
+                for j in range(len(randp[i])):
+                    led.unplot(x + i, y + j)
+        x += 1
+        for i in range(len(randp)):
+            for j in range(len(randp[i])):
+                if(randp[i][j] == 1):
+                    led.plot(x + i, y + j)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+#rotate code
+def on_button_pressed_ab():
+    global randp,x ,y
+    noblock = True
+    temp = [[0,0],[0,0]]
+    temp[0][0] = randp[1][0]
+    temp[1][0] = randp[1][1]
+    temp[1][1] = randp[0][1]
+    temp[0][1] = randp[0][0]
+    for i in range(len(temp)):
+        for j in range(len(temp[i])):
+            if(temp[i][j] == board[x + i][y + j] and temp[i][j] == 1):
+                noblock = False
+    if(noblock):
+        for i in range(len(randp)):
+            for j in range(len(randp[i])):
+                led.unplot(x + i, y + j)
+        randp = temp
+        for k in range(len(randp)):
+            for l in range(len(randp[k])):
+                if(randp[k][l] == 1):
+                    led.plot(x + k, y + l)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
--- main.ts
+++ main.ts
@@ -1,14 +1,37 @@
+// setup code
 let y = 0
-let x = 1
+let x = 2
 let board = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
 let pieces = [[[1, 1], [1, 1]], [[0, 1], [1, 1]], [[1, 1], [0, 1]], [[0, 0], [1, 1]]]
 let rand = randint(0, 3)
+let gameover = false
+let randp = pieces[rand]
+// check if line cleared
+function checkline() {
+    for (let j = 0; j < board[0].length; j++) {
+        for (let i = 0; i < board.length; i++) {
+            if (board[i][j] == 0) {
+                break
+            } else if (i == 4 && board[i][j] == 1) {
+                for (let k = 0; k < 5; k++) {
+                    led.unplot(k, j)
+                    board[k][j] == 0
+                    pause(200)
+                }
+            }
+            
+        }
+    }
+}
+
 basic.forever(function on_forever() {
     let i: number;
     let j: number;
-    let gameover: boolean;
     
-    let randp = pieces[rand]
+    while (gameover) {
+        basic.clearScreen()
+        basic.showIcon(IconNames.Sad)
+    }
     for (i = 0; i < randp.length; i++) {
         for (j = 0; j < randp[i].length; j++) {
             if (randp[i][j] == 1) {
@@ -26,26 +49,130 @@ basic.forever(function on_forever() {
             
         }
     }
-    for (j = 0; j < 2; j++) {
-        if (randp[j][1] == 1 && (y + 1 == 4 || board[x + j][y + 2] == 1)) {
-            if (y <= 1) {
-                gameover = true
+    for (j = 0; j < randp.length; j++) {
+        for (let l = 0; l < randp[j].length; l++) {
+            if (randp[j][l] == 1 && (y + l == 4 || board[x + j][y + l + 1] == 1)) {
+                if (y < 1) {
+                    gameover = true
+                }
+                
+                for (i = 0; i < randp.length; i++) {
+                    for (let k = 0; k < randp[i].length; k++) {
+                        if (randp[i][k] == 1) {
+                            board[x + i][y + k] = 1
+                            led.plot(x + i, y + k)
+                        }
+                        
+                    }
+                }
+                rand = randint(0, 3)
+                y = -1
+                x = 2
+                randp = pieces[rand]
+                checkline()
             }
             
-            for (i = 0; i < randp.length; i++) {
-                for (let k = 0; k < randp[i].length; k++) {
-                    if (randp[i][k] == 1) {
-                        board[x + i][y + k] = 1
-                        led.plot(x + i, y + k)
-                    }
-                    
-                }
-            }
-            rand = randint(0, 3)
-            y = -1
-            x = 1
         }
-        
     }
     y += 1
+})
+// left right code
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    let i: number;
+    let j: number;
+    
+    let move = true
+    for (i = 0; i < randp.length; i++) {
+        for (j = 0; j < randp[i].length; j++) {
+            if (randp[i][j] == 1 && (x + i == 0 || board[x + i - 1][y + j] == 1)) {
+                move = false
+            }
+            
+        }
+    }
+    if (move == true) {
+        for (i = 0; i < randp.length; i++) {
+            for (j = 0; j < randp[i].length; j++) {
+                led.unplot(x + i, y + j)
+            }
+        }
+        x -= 1
+        for (i = 0; i < randp.length; i++) {
+            for (j = 0; j < randp[i].length; j++) {
+                if (randp[i][j] == 1) {
+                    led.plot(x + i, y + j)
+                }
+                
+            }
+        }
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    let i: number;
+    let j: number;
+    
+    let move = true
+    for (i = 0; i < randp.length; i++) {
+        for (j = 0; j < randp[i].length; j++) {
+            if (randp[i][j] == 1 && (x + i == 4 || board[x + i + 1][y + j] == 1)) {
+                move = false
+            }
+            
+        }
+    }
+    if (move == true) {
+        for (i = 0; i < randp.length; i++) {
+            for (j = 0; j < randp[i].length; j++) {
+                led.unplot(x + i, y + j)
+            }
+        }
+        x += 1
+        for (i = 0; i < randp.length; i++) {
+            for (j = 0; j < randp[i].length; j++) {
+                if (randp[i][j] == 1) {
+                    led.plot(x + i, y + j)
+                }
+                
+            }
+        }
+    }
+    
+})
+// rotate code
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    let i: number;
+    let j: number;
+    
+    let noblock = true
+    let temp = [[0, 0], [0, 0]]
+    temp[0][0] = randp[1][0]
+    temp[1][0] = randp[1][1]
+    temp[1][1] = randp[0][1]
+    temp[0][1] = randp[0][0]
+    for (i = 0; i < temp.length; i++) {
+        for (j = 0; j < temp[i].length; j++) {
+            if (temp[i][j] == board[x + i][y + j] && temp[i][j] == 1) {
+                noblock = false
+            }
+            
+        }
+    }
+    if (noblock) {
+        for (i = 0; i < randp.length; i++) {
+            for (j = 0; j < randp[i].length; j++) {
+                led.unplot(x + i, y + j)
+            }
+        }
+        randp = temp
+        for (let k = 0; k < randp.length; k++) {
+            for (let l = 0; l < randp[k].length; l++) {
+                if (randp[k][l] == 1) {
+                    led.plot(x + k, y + l)
+                }
+                
+            }
+        }
+    }
+    
 })
