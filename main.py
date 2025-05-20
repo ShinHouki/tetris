909cb39891e34911f65f08986ddd3a9ae713a8b4
ShinHouki <148901615+ShinHouki@users.noreply.github.com>
2025-05-20 17:07:04 +0800

    added comments to explain process

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#setup code
+#setup x and y of piece start position, board is empty and represents microbit leds, pieces for each possible piece
 y = 0
 x = 2
 board = [[0, 0, 0, 0, 0],
@@ -10,22 +10,29 @@ pieces = [[[1, 1], [1, 1]],
     [[0, 1], [1, 1]],
     [[1, 1], [0, 1]],
     [[0, 0], [1, 1]]]
+#rand picks random piece out of 4 pieces, randp is the picked piece array
 rand = randint(0, 3)
 gameover = False
 randp = pieces[rand]
 #check if line cleared
 
 def checkline():
+    #checks every line to see if there is a line with a line clear
     for j in range(len(board[0])):
         for i in range(len(board)):
+            #line is not fully filled
             if(board[i][j] == 0):
                 break
+            #line is fully filled
             else:
                 if(i == 4 and board[i][j] == 1):
+                    #this loop is to clear filled line
                     for k in range(5):
                         led.unplot(k, j)
                         board[k][j] = 0
+                        #pause for effect
                         pause(200)
+                    #this loop is to move lines above cleared line down 1, top line gets reset with 0
                     for l in range(5):
                         for m in range(j , 0 , -1):
                             if(m == 0):
@@ -43,49 +50,66 @@ def checkline():
 
 def on_forever():
     global x, y, rand, gameover, randp
+    #gameover code would clear screen then show sad face
     while gameover:
         basic.clear_screen()
         basic.show_icon(IconNames.SAD)
+    #plot randp
     for i in range(len(randp)):
         for j in range(len(randp[i])):
             if(randp[i][j] == 1):
                 led.plot(x + i, y + j)
+    #pause to show piece
     pause(1000)
+    #unplot randp
     for i in range(len(randp)):
             for j in range(len(randp[i])):
                 if(randp[i][j] == 1):
                     led.unplot(x + i, y + j)
+    #check if piece would collide with bottom or other board piece
     for j in range(len(randp)):
         for l in range(len(randp[j])):
+            #if statement is true if there is collision
             if(randp[j][l] == 1 and (y + l == 4 or board[x + j][y + l + 1] == 1)):
+                #gameover if new piece cannot fit
                 if(y < 1):
                     gameover = True
+                #change board to have piece on it also plots the piece on board
                 for i in range(len(randp)):
                     for k in range(len(randp[i])):
                         if(randp[i][k] == 1):
                             board[x + i][y + k] = 1
                             led.plot(x + i, y + k)
+                #set up new randp
                 rand = randint(0, 3)
                 y = -1
                 x = 2
                 randp = pieces[rand]
+                #checks if line is clearable, too complex to put in so separated into new function, shouldve done this with more things
                 checkline()
+    #moves piece down one
     y += 1
 basic.forever(on_forever)
 
 #left right code
 def on_button_pressed_a():
     global x, randp
+    #move starts True
     move = True
+    #if collision then don't move
     for i in range(len(randp)):
         for j in range(len(randp[i])):
             if(randp[i][j] == 1 and ( x + i == 0 or board[x + i - 1][y + j] == 1)):
                 move = False
+    #move if there is no collision
     if(move == True):
+        #unplot
         for i in range(len(randp)):
                 for j in range(len(randp[i])):
                     led.unplot(x + i, y + j)
+        #move left
         x -= 1
+        #plot
         for i in range(len(randp)):
             for j in range(len(randp[i])):
                 if(randp[i][j] == 1):
@@ -96,16 +120,21 @@ input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     global x, randp
+    #move starts True
     move = True
+    #if collision then don't move
     for i in range(len(randp)):
         for j in range(len(randp[i])):
             if(randp[i][j] == 1 and ( x + i == 4 or board[x + i + 1][y + j] == 1)):
                 move = False
     if(move == True):
+        #unplot
         for i in range(len(randp)):
                 for j in range(len(randp[i])):
                     led.unplot(x + i, y + j)
+        #move right
         x += 1
+        #plot
         for i in range(len(randp)):
             for j in range(len(randp[i])):
                 if(randp[i][j] == 1):
@@ -116,20 +145,26 @@ input.on_button_pressed(Button.B, on_button_pressed_b)
 def on_button_pressed_ab():
     global randp,x ,y
     noblock = True
+    #set temp to rotated randp
     temp = [[0,0],[0,0]]
     temp[0][0] = randp[1][0]
     temp[1][0] = randp[1][1]
     temp[1][1] = randp[0][1]
     temp[0][1] = randp[0][0]
+    #check if temp would collide with existing board
     for i in range(len(temp)):
         for j in range(len(temp[i])):
             if(temp[i][j] == board[x + i][y + j] and temp[i][j] == 1):
                 noblock = False
+    #if there is no collision
     if(noblock):
+        #unplot
         for i in range(len(randp)):
             for j in range(len(randp[i])):
                 led.unplot(x + i, y + j)
+        #set randp to rotated version
         randp = temp
+        #plot
         for k in range(len(randp)):
             for l in range(len(randp[k])):
                 if(randp[k][l] == 1):
--- main.ts
+++ main.ts
@@ -1,23 +1,29 @@
-// setup code
+// setup x and y of piece start position, board is empty and represents microbit leds, pieces for each possible piece
 let y = 0
 let x = 2
 let board = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
 let pieces = [[[1, 1], [1, 1]], [[0, 1], [1, 1]], [[1, 1], [0, 1]], [[0, 0], [1, 1]]]
+// rand picks random piece out of 4 pieces, randp is the picked piece array
 let rand = randint(0, 3)
 let gameover = false
 let randp = pieces[rand]
 // check if line cleared
 function checkline() {
+    // checks every line to see if there is a line with a line clear
     for (let j = 0; j < board[0].length; j++) {
         for (let i = 0; i < board.length; i++) {
+            // line is not fully filled
             if (board[i][j] == 0) {
                 break
             } else if (i == 4 && board[i][j] == 1) {
+                // this loop is to clear filled line
                 for (let k = 0; k < 5; k++) {
                     led.unplot(k, j)
                     board[k][j] = 0
+                    // pause for effect
                     pause(200)
                 }
+                // this loop is to move lines above cleared line down 1, top line gets reset with 0
                 for (let l = 0; l < 5; l++) {
                     for (let m = j; m > 0; m += -1) {
                         if (m == 0) {
@@ -45,10 +51,12 @@ basic.forever(function on_forever() {
     let i: number;
     let j: number;
     
+    // gameover code would clear screen then show sad face
     while (gameover) {
         basic.clearScreen()
         basic.showIcon(IconNames.Sad)
     }
+    // plot randp
     for (i = 0; i < randp.length; i++) {
         for (j = 0; j < randp[i].length; j++) {
             if (randp[i][j] == 1) {
@@ -57,7 +65,9 @@ basic.forever(function on_forever() {
             
         }
     }
+    // pause to show piece
     pause(1000)
+    // unplot randp
     for (i = 0; i < randp.length; i++) {
         for (j = 0; j < randp[i].length; j++) {
             if (randp[i][j] == 1) {
@@ -66,13 +76,17 @@ basic.forever(function on_forever() {
             
         }
     }
+    // check if piece would collide with bottom or other board piece
     for (j = 0; j < randp.length; j++) {
         for (let l = 0; l < randp[j].length; l++) {
+            // if statement is true if there is collision
             if (randp[j][l] == 1 && (y + l == 4 || board[x + j][y + l + 1] == 1)) {
+                // gameover if new piece cannot fit
                 if (y < 1) {
                     gameover = true
                 }
                 
+                // change board to have piece on it also plots the piece on board
                 for (i = 0; i < randp.length; i++) {
                     for (let k = 0; k < randp[i].length; k++) {
                         if (randp[i][k] == 1) {
@@ -82,15 +96,18 @@ basic.forever(function on_forever() {
                         
                     }
                 }
+                // set up new randp
                 rand = randint(0, 3)
                 y = -1
                 x = 2
                 randp = pieces[rand]
+                // checks if line is clearable, too complex to put in so separated into new function, shouldve done this with more things
                 checkline()
             }
             
         }
     }
+    // moves piece down one
     y += 1
 })
 // left right code
@@ -98,7 +115,9 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
     let i: number;
     let j: number;
     
+    // move starts True
     let move = true
+    // if collision then don't move
     for (i = 0; i < randp.length; i++) {
         for (j = 0; j < randp[i].length; j++) {
             if (randp[i][j] == 1 && (x + i == 0 || board[x + i - 1][y + j] == 1)) {
@@ -107,13 +126,17 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
             
         }
     }
+    // move if there is no collision
     if (move == true) {
+        // unplot
         for (i = 0; i < randp.length; i++) {
             for (j = 0; j < randp[i].length; j++) {
                 led.unplot(x + i, y + j)
             }
         }
+        // move left
         x -= 1
+        // plot
         for (i = 0; i < randp.length; i++) {
             for (j = 0; j < randp[i].length; j++) {
                 if (randp[i][j] == 1) {
@@ -129,7 +152,9 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
     let i: number;
     let j: number;
     
+    // move starts True
     let move = true
+    // if collision then don't move
     for (i = 0; i < randp.length; i++) {
         for (j = 0; j < randp[i].length; j++) {
             if (randp[i][j] == 1 && (x + i == 4 || board[x + i + 1][y + j] == 1)) {
@@ -139,12 +164,15 @@ input.onButtonPressed(Button.B, function on_button_pressed_b() {
         }
     }
     if (move == true) {
+        // unplot
         for (i = 0; i < randp.length; i++) {
             for (j = 0; j < randp[i].length; j++) {
                 led.unplot(x + i, y + j)
             }
         }
+        // move right
         x += 1
+        // plot
         for (i = 0; i < randp.length; i++) {
             for (j = 0; j < randp[i].length; j++) {
                 if (randp[i][j] == 1) {
@@ -162,11 +190,13 @@ input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     let j: number;
     
     let noblock = true
+    // set temp to rotated randp
     let temp = [[0, 0], [0, 0]]
     temp[0][0] = randp[1][0]
     temp[1][0] = randp[1][1]
     temp[1][1] = randp[0][1]
     temp[0][1] = randp[0][0]
+    // check if temp would collide with existing board
     for (i = 0; i < temp.length; i++) {
         for (j = 0; j < temp[i].length; j++) {
             if (temp[i][j] == board[x + i][y + j] && temp[i][j] == 1) {
@@ -175,13 +205,17 @@ input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
             
         }
     }
+    // if there is no collision
     if (noblock) {
+        // unplot
         for (i = 0; i < randp.length; i++) {
             for (j = 0; j < randp[i].length; j++) {
                 led.unplot(x + i, y + j)
             }
         }
+        // set randp to rotated version
         randp = temp
+        // plot
         for (let k = 0; k < randp.length; k++) {
             for (let l = 0; l < randp[k].length; l++) {
                 if (randp[k][l] == 1) {
